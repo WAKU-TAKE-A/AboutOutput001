2a08321889ed984d41691c493556f2de39706447
WAKU-TAKE-A <waku-take-a@ymail.ne.jp>
2023-12-10 22:32:35 +0900

    update

---
+++ AboutOutput001/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboutOutput001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // DOBON.NET様を参考にしました（コピーしました）
+            // https://dobon.net/vb/dotnet/process/standardoutput.html
+
+            //Processオブジェクトを作成
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            //出力を読み取れるようにする
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            //ウィンドウを表示しないようにする
+            p.StartInfo.CreateNoWindow = true;
+            //コマンドラインを指定（"/c"は実行後閉じるために必要）
+            p.StartInfo.Arguments = @"/c dir /b";
+
+            //起動
+            p.Start();
+
+            //出力を読み取る
+            textBox1.Text = p.StandardOutput.ReadToEnd();
+
+            //プロセス終了まで待機する
+            //WaitForExitはReadToEndの後である必要がある
+            //(親プロセス、子プロセスでブロック防止のため)
+            p.WaitForExit();
+            p.Close();
+        }
+    }
+}
---
+++ Reference001Clr/Reference001Clr.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+using namespace System;
+
+namespace Reference001Clr {
+	public ref class Class1
+	{
+		// TODO: このクラスのメソッドをここに追加します。
+	};
+}
